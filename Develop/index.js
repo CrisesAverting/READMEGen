262dadd2209fe1aa05867e0984815ad2a11d7c3e
nicco <mannythemanmoth@gmail.com>
2023-05-29 13:41:43 -0400

    added start code for generate MD file, added code for licensing

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,46 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'Repo Title',
+    message: 'What is the Title of the repository ',
+},
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Describe the purpose of the repository',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'Enter the installation command for your app to install required packages',
+    },
+    {
+        type: 'input',
+        name: 'repo_Contributor',
+        message: 'Enter the contributor requirements',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter your contact Email',
+    },];
 
+
+    
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
